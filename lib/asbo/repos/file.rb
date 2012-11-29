4ae50fd0875fed57054fc8ae4cd28ce7753e539f
Mark Ferry <mark@markferry.net>
2012-11-29 12:08:57 +0000

    File repo publishing

--- lib/asbo/repos/file.rb
+++ lib/asbo/repos/file.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module ASBO::Repo
   class File
     include ASBO::Logger
@@ -16,6 +18,33 @@ module ASBO::Repo
     def download
       raise AppError,  "Can't find package source #{@source}" unless ::File.file?(@path)
       @path
+    end
+
+    def publish(file, overwrite=false)
+      log.debug "Publishing #{@path}"
+
+      begin
+        FileUtils.cd(::File.dirname(@path))
+      rescue SystemCallError => e
+        if e == Errno::ENOENT
+          log.debug "Creating dir: #{::File.dirname(@path)}"
+          FileUtils.mkdir_p(::File.dirname(@path))
+          FileUtils.cd(::File.dirname(@path))
+        else
+          raise ASBO::AppError, "Could not chdir, #{e.message}"
+        end
+      end
+
+      exists = File.file?(::File.basename(@path)) rescue false
+      raise ASBO::AppError, "File #{@path} already exists. Use the appropriate flag to force overwriting" if exists && !overwrite
+
+      log.debug "Uploading..."
+      begin
+        FileUtils.cp(file, ::File.basename(@path))
+      rescue SystemCallError => e
+        raise ASBO::AppError, "Failed to upload file #{@path}: #{e.message}"
+      end
+      log.info "Uploaded #{@path}"
     end
   end
 end
